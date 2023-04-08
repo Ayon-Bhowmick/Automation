fe64e5919a3e4caf283c4407f37e2c06ac0f2dd4
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-04-07 22:30:16 -0400

    made more adaptable

--- CheckBus.py
+++ CheckBus.py
@@ -2,22 +2,24 @@ from requests import get
 from sys import argv
 from json import loads
 from time import sleep
+from notifypy import Notify
 
-ALUMNI_BUILDING = (40.60668, -75.38097)
-GYM = (40.60713, -75.37456)
+notification = Notify()
+locations = {"library": (40.60668, -75.38097),
+                                            "home": (40.60713, -75.37456)}
 
-if argv[1] == "library":
-    location = ALUMNI_BUILDING
-if argv[1] == "home":
-    location = GYM
-
-while 1:
-    r = get("https://lehigh.doublemap.com/map/v2/buses")
-    data = loads(r.text)
-    cords = [(bus["lat"], bus["lon"]) for bus in data]
-    for bus in cords:
-        if bus[0] >= (location[0] - 0.001) and bus[0] <= (location[0] + 0.001):
-            if bus[1] >= (location[1] - 0.001) and bus[1] <= (location[1] + 0.001):
-                print("bus is close")
-                exit()
-    sleep(2)
+if __name__ == "main":
+    location: tuple[float, float] = locations[argv[1]]
+    while 1:
+        r = get("https://lehigh.doublemap.com/map/v2/buses")
+        data = loads(r.text)
+        cords = [(bus["lat"], bus["lon"]) for bus in data]
+        for bus in cords:
+            if bus[0] >= (location[0] - 0.001) and bus[0] <= (location[0] + 0.001):
+                if bus[1] >= (location[1] - 0.001) and bus[1] <= (location[1] + 0.001):
+                    print("bus is close")
+                    notification.title = f"The bus is close to {argv[1]}"
+                    notification.message = f"The bus is nearby"
+                    notification.send()
+                    exit()
+        sleep(2)
